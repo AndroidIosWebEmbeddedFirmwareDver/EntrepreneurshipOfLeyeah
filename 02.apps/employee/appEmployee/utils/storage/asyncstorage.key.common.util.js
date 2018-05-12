a9a5fcde255470b7387738482fce9831ebd5bb23
wangxiaolong <494951601@qq.com>
2018-05-12 10:28:49 +0800

    2018-05-12

--- 02.apps/employee/appEmployee/utils/storage/asyncstorage.key.common.util.js
+++
@@ -1,6 +0,0 @@
-/**
- *
- */
-export default class AsyncstorageKeyCommonUtil {
-    static LOGINKEYID = {key: 'app.employee.login', id: 10002, appType: 2, appTypeName: '雇员'};
-}
--- 02.apps/employer/appEmployer/utils/storage/asyncstorage.key.common.util.js
+++
@@ -1,6 +0,0 @@
-/**
- *
- */
-export default class AsyncstorageKeyCommonUtil {
-    static LOGINKEYID = {key: 'app.employer.login', id: 10001, appType: 1, appTypeName: '雇主'};
-}
